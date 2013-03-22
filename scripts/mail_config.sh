00f837d45e98d7660258f422a7388394e3b3b277
aweiteka <aweiteka@redhat.com>
2013-03-22 08:51:43 -0400

    removed sub-man, yum update, added postfix todo

--- scripts/mail_config.sh
+++ scripts/mail_config.sh
@@ -3,12 +3,10 @@
 # setup inbound mail server for mail processing
 # will forward mail for delivery
  
-subscription-manager register --username=******@redhat.com --password=************** --autosubscribe
-yum update -y
 yum install -y postfix mutt
 postconf -e "inet_interfaces = all"
 postconf -e "mynetworks_style = class"
 postconf -e "mynetworks = 10.0.0.0/8"
-# if sendmail running, kill it
+# TODO: if sendmail running, kill it (bound to port 25)
 service postfix start && chkconfig postfix on
 
