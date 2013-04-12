f00c2a3929f69f1b4a34b6bc9b35290698295a0f
aweiteka <aweiteka@redhat.com>
2013-04-12 17:01:06 -0400

    end-2-end operation support  * create action logs and only returns IP address  * simplified main() and pushed logic up into guest class  * logging more consistent

--- scripts/rhev_api_control.py
+++ scripts/rhev_api_control.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 # Control RHEVM via api
-# * create new guest via template
+# * create new guest from template
+# * add network interface (nic)
 # * start and stop guest
 # * get status and return IP address of running guest
-# * TODO: import OVF and convert to template
+
+# TODO:
+# * filter so less data returned in GET requests
+# * remove VM
 
 from optparse import OptionParser
 import sys
@@ -13,6 +17,7 @@ import time
 import requests
 import urllib
 import logging
+import inspect
 
 
 class Connect(object):
@@ -20,7 +25,8 @@ class Connect(object):
         logfile = "rhev_api.log"
         logger = logging.getLogger()
         fh = logging.FileHandler(logfile, mode='w')
-        formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s", "%b %d %H:%M:%S")
+        formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s",
+                                      "%b %d %H:%M:%S")
         fh.setFormatter(formatter)
         if self.debug:
             logger.setLevel(logging.DEBUG)
@@ -32,19 +38,21 @@ class Connect(object):
     def opts(self):
         parser = OptionParser(usage=self.usage)
         parser.add_option("-o", "--host", dest="host",
-                          help="RHEVM host in the form of https://localhost (without '/api')", 
+                          help="RHEVM host in the form of https://localhost \
+                                (without '/api')",
                           metavar="HOST")
-        parser.add_option("-t", "--template", dest="template", 
-                          default="MIQ-v50029-DND",
+        parser.add_option("-t", "--template", dest="template",
                           help="template name", metavar="TEMP_NAME")
-        parser.add_option("-v", "--vm_name", dest="vm_name", 
+        parser.add_option("-v", "--vm_name", dest="vm_name",
                           help="VM name", metavar="VM_NAME")
         parser.add_option("-a", "--action", dest="action",
-                          choices=["create", "add_nic", "start", "stop", "status"],
-                          help="the action you want to do. create, add_nic, start, stop, status", 
+                          choices=["create", "add_nic", "start", "stop",
+                                   "status"],
+                          help="the action you want to do. create, add_nic, \
+                                start, stop, status",
                           metavar="ACTION")
         parser.add_option("-u", "--user", dest="user",
-                          help="RHEVM password", metavar="USER")
+                          help="RHEVM username", metavar="USER")
         parser.add_option("-p", "--pass", dest="passwd",
                           help="RHEVM password", metavar="PASS")
         parser.add_option("-d", "--debug",
@@ -67,12 +75,15 @@ class Connect(object):
     def usage(self):
         return """
     %prog -o <rhevm_host>
-        -v <vm_name>
         -a <create|add_nic|start|stop|status>
+        -v <vm_name>
+        [-t <template_name>]
         -u <rhevm_user>
         -p <rhevm_passwd>
-        [-t <template_name>]
-        [-d]"""
+        [-d]
+
+   Action 'create' will create <vm_name> from <template_name>, add a NIC
+   on default RHEVM network, start guest and return IP address when running."""
 
     @property
     def host(self):
@@ -90,32 +101,58 @@ class Connect(object):
     def debug(self):
         return self.opts.debug
 
+    def pretty_json(self, j):
+        return json.dumps(j, sort_keys=True, indent=4, separators=(',', ': '))
+
     def get(self, url):
         """Generic get request
         """
-        r = requests.get(url,
-                         headers=self.headers,
-                         auth=(self.user, self.passwd), 
-                         verify=False)
-        logging.debug(r.text)
-        if not self.success(r):
-            return False
-        else:
-            return json.loads(r.text)
+        try:
+            r = requests.get(url,
+                             headers=self.headers,
+                             auth=(self.user, self.passwd),
+                             verify=False)
+            if self.success(r):
+                j = r.json
+                logging.debug(self.pretty_json(j))
+                assert len(j['vms']) == 1, "Unexpected number of VMs returned in search"
+                return j
+        except KeyError as e:
+            logging.exception("No VMs reaturned in search. Broaden vm_name with wildcard '-v %s*' and retry?" % (self.vm_name))
+            raise e
+        except AssertionError as e:
+            logging.exception("VM search too broad. Refine vm_name '%s' and retry" % (self.vm_name))
+            raise e
 
     def post(self, url, payload):
         """Generic post request
         """
-        r = requests.post(url,
-                          headers=self.headers,
-                          auth=(self.user, self.passwd),
-                          verify=False,
-                          data=payload)
-        logging.debug(r.text)
-        if not self.success(r):
-            return False
+        try:
+            r = requests.post(url,
+                              headers=self.headers,
+                              auth=(self.user, self.passwd),
+                              verify=False,
+                              data=payload)
+            if self.success(r):
+                j = r.json
+                logging.debug(self.pretty_json(j))
+                return j
+        except Exception, e:
+            logging.exception("POST error: %s" % e)
+            raise e
+
+    def success(self, r):
+        """Validate request
+        """
+        # reqeusts built-in exception handler. Is None if okay
+        r.raise_for_status()
+        try:
+            assert r.headers['content-type'] == "application/json", \
+                "Reponse is not JSON format"
+        except AssertionError as e:
+            logging.exception(e)
         else:
-            return json.loads(r.text)
+            return True
 
     @property
     def headers(self):
@@ -134,14 +171,16 @@ class Connect(object):
 
     @property
     def create_vm_param(self):
+        # FIXME: add params for memory/cpus?
         """Post request payload for creating vm
         """
         param = """\
 <vm><name>{name}</name><memory>{memory}</memory><cpu><topology cores='{cpus}' sockets='1'/></cpu><cluster><name>{cluster}</name></cluster><action>start</action><template><name>{template}</name></template></vm>"""
-        return param.format(name=self.vm_name, 
-                            memory="6442450944", # bytes
-                            cpus="4", 
-                            cluster="iscsi", 
+        return param.format(name=self.vm_name
+                            # in bytes
+                            memory="6442450944",
+                            cpus="4",
+                            cluster="iscsi",
                             template=self.template)
 
     @property
@@ -161,29 +200,16 @@ class Connect(object):
 class Guest(Connect):
 
     @property
-    def template(self):
-        return self.opts.template
+    def action(self):
+        return self.opts.action
 
     @property
     def vm_name(self):
         return self.opts.vm_name
 
     @property
-    def action(self):
-        return self.opts.action
-
-    @property
-    def details(self):
-        """Guest details
-        """
-        details = self.get(self.search_vm_url)
-        return self.format_details(details)
-
-    def format_details(self, d):
-        form = "Guest name: {name}\nID: {vmid}\nStatus: {status}"
-        return form.format(name=d['vms'][0]['name'], 
-                           vmid=d['vms'][0]['id'], 
-                           status=d['vms'][0]['status']['state'])
+    def template(self):
+        return self.opts.template
 
     @property
     def name(self):
@@ -191,13 +217,6 @@ class Guest(Connect):
         """
         r = self.get(self.search_vm_url)
         return r['vms'][0]['name']
-
-    @property
-    def id(self):
-        """Guest id
-        """
-        r = self.get(self.search_vm_url)
-        return r['vms'][0]['id']
 
     @property
     def nic_url(self):
@@ -225,16 +244,57 @@ class Guest(Connect):
         """Guest IP address
         """
         r = self.get(self.search_vm_url)
-        if "guestInfo" in r['vms'][0]:
+        try:
             return r['vms'][0]['guestInfo']['ips']['ips'][0]['address']
+        except KeyError as e:
+            pass
 
     def print_ip(self):
         """Print IP if not None
         """
-        ip = vm.ip_addr
-        if ip is not None:
-            logging.info("IP: " + ip)
-            return ip
+        status = self.status
+        if status == "up":
+            ip = self.ip_addr
+            if ip is None:
+                logging.info("VM is %s but ip is %s" % (status, ip))
+                print status
+            else:
+                logging.info("VM is %s. IP: %s" % (status, ip))
+                print ip
+        else:
+            logging.info(status)
+            print status
+
+    def verify(self, assertion):
+        """General function to verify stages of VM
+           assertion is one of create|up|down
+        """
+        max_attempts = 40
+        sleep_interval = 15 
+        for attempt in range(1, max_attempts+1):
+            try:
+                if assertion in "create":
+                    assert self.status == "down", "Creating VM"
+                elif assertion in "up":
+                    assert self.status == "up", "powering up"
+                    assert self.ip_addr is not None, "no IP address"
+                elif assertion in "down":
+                    assert self.status == "down", "shutting down"
+            except AssertionError as e:
+                if attempt < max_attempts:
+                    logging.info("Waiting for VM (%s/%s): %s" % (attempt, max_attempts, e))
+                    time.sleep(sleep_interval)
+                    pass
+                # Enough sleeping, something went wrong
+                else:
+                    logging.exception("Verify failed. Max attempts: %s" % max_attempts)
+                    raise e
+            else:
+                # No exceptions raised
+                logging.info("verified vm %s" % assertion)
+                break
+
+        return True
 
     @property
     def status(self):
@@ -243,60 +303,65 @@ class Guest(Connect):
         r = self.get(self.search_vm_url)
         return r['vms'][0]['status']['state']
 
-    def is_up(self, status):
-        """True if status is up
-        """
-        if status == "up":
-            return True
+    def create_vm_from_template(self):
+        self.post(self.post_url, self.create_vm_param)
+        while not vm.verify("create"):
+            pass
         else:
-            return False
-
-    def success(self, results):
-        """Return request status
-        """
-        if results.status_code >= 400:
-            return False
+            vm.add_nic()
+        vm.start()
+        while not vm.verify("up"):
+            pass
         else:
-            return results.status_code
-
-    def new_vm(self):
-        return self.post(self.post_url, self.create_vm_param)
+            vm.print_ip()
 
     def add_nic(self):
         url = self.host + self.nic_url
-        return self.post(url, self.add_nic_param)
+        r = self.post(url, self.add_nic_param)
+        assert r['active'] is True
+        logging.info("Successfully added network interface (NIC): %s %s" %
+                    (r['name'], r['mac']['address']))
 
     def start(self):
         url = self.host + self.start_url
-        return self.post(url, self.null_param)
+        r = self.post(url, self.null_param)
+        #print self.pretty_json(r)
+        assert r['status']['state'] == "complete"
+        logging.info("Successfully requested guest start")
 
     def stop(self):
         url = self.host + self.stop_url
-        return self.post(url, self.null_param)
+        r = self.post(url, self.null_param)
+        assert r['status']['state'] == "complete"
+        logging.info("Successfully requested guest stop")
 
 
 def main():
     vm = Guest()
 
     if vm.action == "create":
-        logging.info(vm.new_vm())
-        # TODO: check for nic
+        vm.create_vm_from_template()
+
     elif vm.action == "add_nic":
-        logging.info(vm.add_nic())
+        vm.add_nic()
+
     elif vm.action == "status":
-        logging.info("Guest name: " + vm.name)
-        logging.info("Guest ID: " + vm.id)
-        print vm.status
-        if vm.status == "up":
-            vm.print_ip()
+        vm.print_ip()
+
     elif vm.action == "start":
-        logging.info(vm.start())
-        # TODO: loop until status is up, print ip addr
-        if vm.status == "up":
+        vm.start()
+        while not vm.verify("up"):
+            pass
+        else:
             vm.print_ip()
+
     elif vm.action == "stop":
-        logging.info(vm.stop())
-        # TODO: validate stop
+        vm.stop()
+        while not vm.verify("down"):
+            pass
+        else:
+            print vm.status
+
 
 if __name__ == '__main__':
     main()
